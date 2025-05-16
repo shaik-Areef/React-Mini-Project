03709795078c6fe3a80f5676800b67bdf8c0cbf3
shaik-Areef <skareef403@gmail.com>
2025-05-16 11:27:30 +0530

    Initial commit

---
+++ projectpilot/src/projects/ProjectsPage.jsx
@@ -0,0 +1,16 @@
+
+import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
+
+
+function ProjectsPage() {
+    return (
+        <>
+            <h1>Projects</h1>
+
+            <ProjectList projects={MOCK_PROJECTS} />
+        </>
+    );
+}
+
+export default ProjectsPage;
